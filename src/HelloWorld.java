42c850f390893c0344fb65fc1c7ebb5b52f32d5e
Aldanis Vigo <aldanisvigo@gmail.com>
2022-11-07 18:44:11 -0600

    Printed out my favorite number

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Helloooo World!!");
+        int myFavorityNumber = 2;
+        System.out.println(myFavorityNumber);
     }
 }
