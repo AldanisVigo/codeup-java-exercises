b47aecf034acc4804d1fc3330b0696cf542c2c13
Aldanis Vigo <aldanisvigo@gmail.com>
2022-11-09 06:44:59 -0600

    StringExercise

---
+++ src/StringExercise.java
@@ -0,0 +1,12 @@
+public class StringExercise {
+    public static void main(String[] args){
+//        Create a class named StringExercise with a main method.
+//
+//        For each of the following output examples: create a String variable that contains the desired output
+//        and print it out to the console, you can do this with only one String variable and one print statement
+//        for each output example.
+
+        String myString = "We don't need no education.\nWe don't need no thought control";
+        System.out.println(myString);
+    }
+}
