6e820610e2432bc7fbd615f18dfbac1f461e56fd
Aldanis Vigo <aldanisvigo@gmail.com>
2022-11-08 21:48:48 -0600

    the value of pi is approximately 3.14

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,10 @@
+public class ConsoleExercises {
+    public static void main(String[] args){
+        // Copy this code into your main method:
+        double pi = 3.14159;
+        // Write some Java code that uses the variable pi to output the following:
+        // The value of pi is approximately 3.14.
+        System.out.printf("The value of pi is approximately %.2f",pi);
+
+    }
+}
