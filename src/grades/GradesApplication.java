a45b6d5cad0661eee0641a5175d7bd78ade8ace5
Aldanis Vigo <aldanisvigo@gmail.com>
2022-11-19 13:41:30 -0600

    Create the command line interface

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,7 +1,12 @@
 package grades;
 
+import util.Input;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.BiConsumer;
 
 public class GradesApplication {
 //    Create a map for students and GitHub usernames
@@ -45,5 +50,57 @@ public class GradesApplication {
         students.put("jkrsak",s2);
         students.put("alvinmckenzie",s3);
         students.put("mickeymouse",s4);
+
+        Scanner myScanner = new Scanner(System.in);
+
+        boolean outterloop = true;
+
+        while(outterloop) {
+            boolean loop = true;
+            Student selectedStudent = null;
+            while (loop) {
+                // Print the list of GitHub usernames out to the console, and ask the user which student they would like
+                // to see more information about.The user should enter a GitHub username (i.e. one of the keys in your map).
+                System.out.println("Which student would you like to see more information about?");
+
+                //Iterate through the student's key set
+                for (Object studentId : students.keySet()) {
+                    //Get the student that corresponds to the current key in the keyset
+                    Student currentStudent = (Student) students.get(studentId);
+
+                    //Print out the github username - and the user's name
+                    System.out.printf("%s - %s\n", studentId, currentStudent.getName());
+                }
+
+
+                //Ask the user to enterthe username of the student they want more info about
+                System.out.print("Type in the github id of the student you want more info about:");
+
+                //Grab what they typed in
+                String selectedStudentKey = myScanner.nextLine();
+
+                // If the given input does not match up with a key in your map, tell the user that no users with that username
+                // were found. If the given username does exist, display information about that student, including their name
+                // and their grades.
+
+                //Check if what they typed in is a key in the students map
+                if (students.containsKey(selectedStudentKey)) {
+                    selectedStudent = (Student) students.get(selectedStudentKey);
+                    loop = false; //If it is, then stop looping.
+                }
+            }
+
+            if (selectedStudent != null) {
+                System.out.printf("%s's average grade is %f\n", selectedStudent.getName(), selectedStudent.getGradeAverage());
+
+                // After the information is displayed, the application should ask the user if they want to continue,
+                // and keep running so long as the answer is yes.
+                Input myInput = new Input(myScanner);
+                boolean wouldLikeToContinue = myInput.yesNo("Would you like to continue?".describeConstable());
+                if(!wouldLikeToContinue){
+                    outterloop = false;
+                }
+            }
+        }
     }
 }
